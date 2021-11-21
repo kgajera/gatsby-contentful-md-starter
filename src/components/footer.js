7762d932a33935655b6e64d04b6e5e3d55d97761
Kishan Gajera <me@kgajera.com>
2021-11-21 13:24:24 -0500

    style: add newline before component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Container from './container';
+
 const Footer = () => {
   return (
     <Container as="footer" className="border-t py-5 mt-10 text-sm text-center">
